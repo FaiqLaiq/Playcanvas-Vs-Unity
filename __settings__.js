0ed136019237d268dd5b5404c49a986f739e0257
FaiqLaiq <faiqlaiq5@gmail.com>
2024-11-10 19:51:25 +0500

    playcanvas

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2104038.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': true,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 202234194, 202235785, 202235786, 202235787, 202237287, 202237369, 202237370, 202237371, 202237372, 202237413 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/202235830/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/202235831/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/202235829/1/ammo.js', 'preload' : true},
+];
